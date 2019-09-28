3ef7ceba695a5631e44d35d61ac4a396e8a807d6
senbinam <senbinam@gmail.com>
2019-09-28 16:25:04 +0545

    hacknews

--- src/components/HackerNews.vue
+++ src/components/HackerNews.vue
@@ -5,7 +5,7 @@
         <h4 class="title">Netflix Ratings</h4>
       </div>
       <div class="search">
-        <input type="text" class="form-control" placeholder="Search by title" />
+        <input v-model="title" type="text" class="form-control" placeholder="Search by title" />
       </div>
       <div class="content">
         <div>
@@ -16,9 +16,11 @@
             <p></p>
           </div>
           <ul class="list">
-            <li>
-              <a href></a>
-              <span>By:</span>
+            <li v-for="(item,index) in news" :key="index">
+              <template v-if="item.title">
+                <a v-bind:href="item.url" target="_blank">{{item.title}}</a>
+                <span>By:{{item.author}}</span>
+              </template>
             </li>
           </ul>
         </div>
@@ -28,8 +30,39 @@
 </template>
 
 <script>
+import axios from "axios"
 export default {
-  name: "HackerNews"
+  name: "HackerNews",
+  data(){
+    return{
+      news: [],
+      title: ""
+    };
+  },
+  async created(){
+    try{
+      const response = await axios.get("http://hn.algolia.com/api/v1/search");
+      this.news = response.data.hits;
+      console.log(response);
+    } catch(error){
+
+    }
+  },
+  watch:{
+    async title(value){
+      try{
+      const response = await axios.get("http://hn.algolia.com/api/v1/search",{
+        params:{
+          query: value
+        }
+      });
+      this.news = response.data.hits;
+      console.log(response);
+    } catch(error){
+
+    }
+    }
+  }
 };
 </script>
 
